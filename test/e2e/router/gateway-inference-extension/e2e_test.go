b2378062c5a7ba7cdffdcf1854e7155ef84fa63a
YaoZengzeng <yaozengzeng@huawei.com>
2025-12-24 11:53:31 +0800

    fix failure
    Signed-off-by: YaoZengzeng <yaozengzeng@huawei.com>

--- test/e2e/router/gateway-inference-extension/e2e_test.go
+++ test/e2e/router/gateway-inference-extension/e2e_test.go
@@ -134,5 +134,5 @@ func TestGatewayInferenceExtension(t *testing.T) {
 		utils.NewChatMessage("user", "Hello GIE"),
 	}
 
-	utils.CheckChatCompletions(t, "deepseek-r1-1-5b", messages)
+	utils.CheckChatCompletions(t, "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B", messages)
 }
